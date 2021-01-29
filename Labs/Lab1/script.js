8218c37d9845f6e33216c35701a3a8c8ab855697
antferna <antferna@indiana.edu>
2021-01-28 20:57:58 -0600

    Lab finished

---
+++ Labs/Lab1/script.js
@@ -0,0 +1,25 @@
+function setup(){
+
+    var teal = "#32a8a8"; 
+
+    var purple = "#342cc9";
+
+
+    createCanvas(800,600);
+  
+    background(teal);
+
+    
+  fill("#C750AB");
+    rect(100,30,80,10);
+  
+    fill(purple);
+    circle(30,60,50);
+
+    stroke(2);
+    line(10,20,200,20); 
+    
+    fill("#c5c5d1");
+    quad(38, 31, 86, 20, 69, 63, 30, 76);
+
+  }
