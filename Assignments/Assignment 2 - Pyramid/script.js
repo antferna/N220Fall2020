6e012c5b21312f6fb25c8df60b7d358a2e0d5f5a
antferna <antferna@indiana.edu>
2021-02-11 22:54:25 -0600

    comments on live

--- Assignments/Assignment 2 - Pyramid/script.js
+++ Assignments/Assignment 2 - Pyramid/script.js
@@ -13,10 +13,10 @@ function draw() {
     background(200);
     fill(255, 50, 0);
   
-    // for each row
+    // Loop for each row
     for (let row = 0; row <= no_of_rows; row++) {
       
-        // for each column in the row
+        //Nested Loop for each column in the row
         for (let column = 0; column <= row ; column ++) {
            square(column * 50, row * 50, 50);
         }
