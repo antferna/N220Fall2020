bbf328cb8c6bc14246e037e4b95432e934ae104d
antferna <antferna@indiana.edu>
2021-03-11 22:19:02 -0600

    name put on

--- Assignments/Assignment 5 - Data Driven/script.js
+++ Assignments/Assignment 5 - Data Driven/script.js
@@ -1,6 +1,10 @@
+//Antonio Fernandez 
+//Assignent 5 
+//Below you can see tht I used object method to draw a circle with radius and positions defined in it. 
 var myCircle = {
     x: 45,
     y: 20,
+    r:20,
     color: [66, 141, 245]
 }
 
@@ -10,5 +14,5 @@ function setup() {
 
 function draw(){
     fill(myCircle.color);
-    circle(myCircle.x,myCircle.y,20);
+    circle(myCircle.x,myCircle.y,myCircle.r);
 }
