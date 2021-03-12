5edcda77c530653b07f87419f428a88003706c80
antferna <antferna@indiana.edu>
2021-03-11 21:39:34 -0600

    Animated Circle

---
+++ Assignments/Assignment 5 - Animated Circle/script.js
@@ -0,0 +1,16 @@
+var myCircle = {
+    x: 45,
+    y: 20,
+    color: [66, 141, 245]
+}
+
+function setup() {
+    createCanvas(400,300)
+}
+
+function draw(){
+    fill(myCircle.color);
+    myCircle.x += 1;
+    myCircle.y += 1;
+    circle(myCircle.x,myCircle.y,20);
+}
